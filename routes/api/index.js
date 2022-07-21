cb250d3a2f4c0e9c7066e91041c7dd527f5f9835
louieiply <louieiply@gmail.com>
2022-07-21 21:16:06 +0930

    Deploy social network API

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const thoughtRoute = require("./thoughtRoute");
 const userRoute = require("./userRoute");
-
+// api/users
 router.use("/users", userRoute);
+// api/thoughts
 router.use("/thoughts", thoughtRoute);
 
 module.exports = router;
